dfa99971b4e83f5100c3d6eeb9af138c530c0241
Ramu148 <143690453+Ramu148@users.noreply.github.com>
2024-07-16 17:57:19 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,52 @@
+//1.creating an array
+
+ let fruit=["Apple","Orange","Graps"]
+ console.log(fruit);
+
+
+//2.Array manipulation
+//Push adding
+ let fruits=["Apple","Orange","Graps"]
+fruit.push("Bananna");
+console.log(fruits);
+
+//pop remove
+
+let fruit1=["Apple","Orange","Graps"]
+fruit1.pop("Graps");
+console.log(fruit1);
+
+//shift
+
+let fruit2=["Apple","Orange","Graps"]
+ fruit2.shift("Apple");
+ console.log(fruit2);
+
+//unshift
+
+let fruit3=["Apple","Orange","Graps"]
+ fruit3.unshift("Apple");
+ console.log(fruit3);
+
+
+//3.array searching
+
+     //6.array sorting
+     //reverse
+     const num3=[2,4,5,7]
+      num3.reverse();
+      document.write(num3);
+
+    //   //sort
+      const fruit4=["Apple","Orange","Mango"]
+     fruit4.sort();
+     console.log(fruit4);
+
+     //5.array mapping
+
+     const value=[2,5,23,53,34];
+     const val=value.map(Math.sqrt);
+     console.log(val);
+
+
+
